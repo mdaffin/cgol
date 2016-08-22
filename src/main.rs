4eb90471cbf7c17f10c0c27063a195c65103ef4d
Michael Daffin <michael.daffin@switchconcepts.com>
2016-08-22 14:09:55 +0100

    Moves the example to be the offical bin

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,21 @@ fn main() {
     let mut paused = true;
     engine.randomise();
 
-
     'main: loop {
         let row = term.rows() - 1;
-        let col = term.cols() - 7;
+        let paused_col = term.cols() - 7;
+        let mut alive: u32 = 0;
+        let mut dead: u32 = 0;
+
         while let Some(Event::Key(ch)) = term.get_event(Duration::new(0, 200)).unwrap() {
             match ch {
                 'q' => break 'main,
                 'p' => paused = !paused,
                 'n' => {engine.tick(); break},
                 'r' => engine.randomise(),
-                e => term.printline(0, row - 1, &format!("{:?}", e)),
+                _ => {},
             }
         }
-        let mut alive: u32 = 0;
-        let mut dead: u32 = 0;
         for (x, y, state) in engine.iter() {
             if state == State::Alive {
                 alive += 1;
@@ -36,13 +36,12 @@ fn main() {
             }
             term.printline(x, y, &format!("{}", state));
         }
-        term.printline(0, row, &format!("Iterations: {}    Alive: {}    Dead: {}", engine.iterations(), alive, dead));
-        term.printline(0, row, &format!("Iterations: {}    Alive: {}    Dead: {}", engine.iterations(), alive, dead));
+        term.printline(0, row, &format!("Iterations: {:10} | Alive: {:10} | Dead: {}", engine.iterations(), alive, dead));
         if !paused {
             engine.tick(); 
-            term.printline(col, row, "      ");
+            term.printline(paused_col, row, "      ");
         } else {
-            term.printline(col, row, "Paused");
+            term.printline(paused_col, row, "Paused");
         }
         term.swap_buffers().unwrap();
     }
