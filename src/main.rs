92f2cb48a9c19f6b01cf4f848cbba492ee99d4dd
Michael Daffin <michael@daffin.io>
2016-08-22 18:39:09 +0100

    Changes display char to ■ and defaults the size to the term size

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use rustty::ui::Painter;
 
 fn main() {
     let mut term = Terminal::new().unwrap();
-    let mut engine = Engine::new(80, 40);
+    let mut engine = Engine::new(term.cols(), term.rows() - 1);
     let mut paused = true;
     engine.randomise();
 
@@ -23,9 +23,12 @@ fn main() {
             match ch {
                 'q' => break 'main,
                 'p' => paused = !paused,
-                'n' => {engine.tick(); break},
+                'n' => {
+                    engine.tick();
+                    break;
+                }
                 'r' => engine.randomise(),
-                _ => {},
+                _ => {}
             }
         }
         for (x, y, state) in engine.iter() {
@@ -34,11 +37,21 @@ fn main() {
             } else {
                 dead += 1;
             }
-            term.printline(x, y, &format!("{}", state));
+            term.printline(x,
+                           y,
+                           match state {
+                               State::Alive => "◼",
+                               State::Dead => " ",
+                           });
         }
-        term.printline(0, row, &format!("Iterations: {:10} | Alive: {:10} | Dead: {}", engine.iterations(), alive, dead));
+        term.printline(0,
+                       row,
+                       &format!("Iterations: {:10} | Alive: {:10} | Dead: {}",
+                                engine.iterations(),
+                                alive,
+                                dead));
         if !paused {
-            engine.tick(); 
+            engine.tick();
             term.printline(paused_col, row, "      ");
         } else {
             term.printline(paused_col, row, "Paused");
